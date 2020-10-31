d47b6249be092cbfe4d550860bb9cbdb2272d76a
Hsuan-Shao Chang <hsuanshao@gmail.com>
2020-10-31 12:20:48 +0800

    [misc] a small example

--- app/main.go
+++ app/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"flag"
+
+	"github.com/hsuanshao/pwd-verify/app/infra/di/deps"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	flag.Parse()
+
+	pv := deps.GetPasswordValidator()
+	_, _, _, _, _, _, err := pv.Validator("a3dh9sa3s")
+
+	if err != nil {
+		logrus.WithField("err", err.Error())
+	}
+
 }
